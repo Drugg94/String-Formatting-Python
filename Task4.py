e349d0cc35ee1d029b88be24df17a1f526260e65
Derek <derek.ruggirello@gmail.com>
2022-02-25 23:56:12 -0500

    Update Task4.py

--- Task4.py
+++ Task4.py
@@ -1,6 +1,39 @@
 # Revision number 1 25Feb2022
 ## Begin Derek Ruggirello here 25Feb2022
 
+first = "3"
+
+first = int(first)
+
+result = first + 3
+
+print(result)
+
+first = "3"
+
+first = float(first)
+
+result = first + 3
+
+print(result)
+
+first = "Hello"
+
+second = 4
+
+print(first + str(second))
+
+first = int(input("Please enter two numbers: "))
+
+second = int(input("number 2: "))
+
+print("The product of", first, "and", second, "is", float((first * second)))
+
+first = "How are you"
+
+print("The letter w is in the", first.find('w') + 1, "place of", first)
+
+
 
 # Revision number 1 25Feb2022
 ## End Derek Ruggirello
