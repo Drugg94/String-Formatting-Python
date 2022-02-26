80fc76fa74bf8e364138edcdc2d4c052f35c1470
Derek <derek.ruggirello@gmail.com>
2022-02-25 23:13:26 -0500

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,7 +1,26 @@
 # Revision number 1 25Feb2022
 ## Begin Derek Ruggirello here 25Feb2022
 
+first = "This is a long string"
 
+print(len(first))
+
+second = "for the python language."
+
+result = first + second
+
+print(result)
+
+first = "Result"
+second = "This"
+
+result = first + " " + second
+
+print(result)
+
+first = "bazinga"
+
+print(first[2:6])
 # Revision number 1 25Feb2022
 ## End Derek Ruggirello
 # Zion Worship Cult / Ram Vuduku / Rich Eissen / the Zion Project
