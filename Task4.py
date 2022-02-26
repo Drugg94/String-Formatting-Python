68363042c2f04e07113bf2abda7c0f2f81b9e9cd
Derek <derek.ruggirello@gmail.com>
2022-02-25 23:03:25 -0500

    Template

---
+++ Task4.py
@@ -0,0 +1,7 @@
+# Revision number 1 25Feb2022
+## Begin Derek Ruggirello here 25Feb2022
+
+
+# Revision number 1 25Feb2022
+## End Derek Ruggirello
+# Zion Worship Cult / Ram Vuduku / Rich Eissen / the Zion Project
