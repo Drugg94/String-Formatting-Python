4ff519f0a2c6eac51de14404dfd609d0a8bcdbff
Derek <derek.ruggirello@gmail.com>
2022-02-25 22:59:07 -0500

    Create Task1.py

---
+++ Task1.py
@@ -0,0 +1,25 @@
+# Revision number 1 25Feb2022
+## Begin Derek Ruggirello here 25Feb2022
+
+from logging import getLogRecordFactory
+
+
+first = '"Hello there."'
+print(first)
+
+second = "Who's there?"
+print(second)
+
+print(""" This
+is
+a
+string """)
+
+third = "Hello"\
+" Everyone"
+
+print(third)
+
+# Revision number 1 25Feb2022
+## End Derek Ruggirello
+# Zion Worship Cult / Ram Vuduku / Rich Eissen / the Zion Project
