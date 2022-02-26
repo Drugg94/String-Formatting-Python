7bfc08de2654783ddc3bbda48d4a96b1a8bff759
Derek <derek.ruggirello@gmail.com>
2022-02-25 23:44:19 -0500

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -1,6 +1,44 @@
 # Revision number 1 25Feb2022
 ## Begin Derek Ruggirello here 25Feb2022
 
+Array = ["Animals", "Badger", "Honey Bee", "Honey Badger"]
+iterator = 0
+for x in Array:
+    Array[iterator] = x.lower()
+    iterator += 1
+for x in Array:
+    print(x)
+
+iterator = 0
+for x in Array:
+    Array[iterator] = x.upper()
+    iterator += 1
+for x in Array:
+    print(x)
+
+string1 = " Filet Mignon"
+string2 = "Brisket "
+string3 = " Cheeseburger "
+
+string1 = string1.lstrip()
+string2 = string2.rstrip()
+string3 = string3.lstrip()
+string3 = string3.rstrip()
+
+print(string1)
+print(string2)
+print(string3)
+
+string1 = "Becomes"
+string2 = "becomes"
+string3 = "BEAR"
+string4 = " bEautiful"
+
+print(string1.startswith("be"))
+print(string2.startswith("be"))
+print(string3.startswith("be"))
+print(string4.startswith("be"))
+
 
 # Revision number 1 25Feb2022
 ## End Derek Ruggirello
